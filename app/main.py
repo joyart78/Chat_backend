076107c3183efb59c63907895d652261da2c9021
KuznetsovAN <kuznetsovan@uriit.ru>
2026-04-24 19:35:33 +0500

    update cors: for all

--- app/main.py
+++ app/main.py
@@ -16,15 +16,10 @@ default_errors = {
 }
 
 
-origins = [
-    "http://localhost",
-    "http://77.91.94.81",
-]
-
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=origins,
-    allow_credentials=True,
+    allow_origins=["*"],
+    allow_credentials=False,
     allow_methods=["*"],
     allow_headers=["*"],
 )
