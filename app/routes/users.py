ea160ee75ba48c6828760f7bed7f1ec4b1215b38
KuznetsovAN <kuznetsovan@uriit.ru>
2026-04-25 12:49:11 +0500

    add user filters

--- app/routes/users.py
+++ app/routes/users.py
@@ -1,5 +1,6 @@
 import fastapi
 from fastapi import APIRouter
+from sqlalchemy import select, desc, asc
 from sqlalchemy.ext.asyncio import AsyncSession
 
 from dependencies.auth import user_info_dep
@@ -20,7 +21,14 @@ async def get_users(
     author: UserInfo = fastapi.Depends(user_info_dep),
 ) -> UserList:
     """Получить список пользователей"""
-    users_, count = await pagination_async(session, model_class=User, **filter_params.model_dump())
+    query = select(User)
+    if filter_params.search:
+        query = query.where(User.login.ilike(f"%{filter_params.search}%"))
+    if filter_params.sort_by:
+        order_direction = desc if filter_params.descending else asc
+        query = query.order_by(order_direction(getattr(User, filter_params.sort_by)))
+
+    users_, count = await pagination_async(session, model_class=User, query=query, **filter_params.model_dump())
 
     data = [await UserInfoOut.model_validate_async(user, session) for user in users_]
 
--- app/schemas/users.py
+++ app/schemas/users.py
@@ -10,6 +10,9 @@ from schemas.shared import UserInfoOut
 class UserFilterParams(Model):
     page: Annotated[int, fastapi.Query(description="Страница")] = 1
     rows_per_page: Annotated[int, fastapi.Query(description="Кол-во записей на странице")] = 25
+    search: Annotated[str | None, fastapi.Query(description="Поиск пользователя")] = None
+    sort_by: Annotated[str | None, fastapi.Query(description="Сортировка")] = "id"
+    descending: Annotated[bool, fastapi.Query(description="Порядок сортировки")] = False
 
 
 class UserList(UserFilterParams):
